2af5abd0700a5f949c65f9dc4ffa8e936db0735f
Anton Hlinisty <anton_hlinisty@epbyminw6012.minsk.epam.com>
2019-10-30 12:37:00 +0300

    Print 'MYVAR' env var to the console

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ sudo apt install npm
 nmp install (if needed)
 
 npm run run-me-now
+
+## Super-duper killer features
+When running the app prints 'MYVAR' env variable to the console
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const https = require('https');
 https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
   let data = '';
 
-  // A chunk of data has been recieved.
+  // A chunk of data has been received.
   resp.on('data', (chunk) => {
     data += chunk;
   });
@@ -13,9 +13,10 @@ https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
   // The whole response has been received. Print out the result.
   resp.on('end', () => {
     console.log(JSON.parse(data).explanation);
+    console.log('---\nprinting the \'MYVAR\' env var:');
+    console.log(process.env.MYVAR);
   });
 
 }).on("error", (err) => {
   console.log("Error: " + err.message);
 });
-
